0062c6c73fc8af6be197fdd8f55c4b137dd4393d
Anna-prog-20 <64792631+Anna-prog-20@users.noreply.github.com>
2020-07-03 01:16:45 +0900

    Delete DirectionalList.java

--- DirectionalList.java
+++
@@ -1,8 +0,0 @@
-public interface DirectionalList {
-    void add(String val);
-    boolean remove(String val);
-    Object getFirst();
-//    Object getPrevious();
-    int size();
-    void print();
-}
